5d90bb041d3c335fbf6cdf5a909fdb886f67a83a
Alex <alex@alex-VirtualBox.(none)>
2016-10-16 12:51:07 +0300

    add validates of vacancy_id

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,5 +1,8 @@
 class Vacancy < ActiveRecord::Base
 
-    # Если ссылка не уникальна, то объект не сохранится в базу
-  validates :link, uniqueness: true
+  # Если vacancy_id не представлен, то объект не сохранится в базу
+  validates :vacancy_id, presence: true
+
+  # Если vacancy_id не уникален, то объект не сохранится в базу
+  validates :vacancy_id, uniqueness: true
 end
