aa2fd93150222b065536e315af7f53eb8302a519
Alex <alex@alex-VirtualBox.(none)>
2016-10-16 13:36:04 +0300

    comments for jobs

--- app/jobs/vacancies_handling_job.rb
+++ app/jobs/vacancies_handling_job.rb
@@ -1,12 +1,16 @@
+# Обработка списка вакансий Ruby разработчика в СПб
+
 class VacanciesHandlingJob
   @queue = :simple
 
   require 'open-uri'
 
   def self.perform
+    # Получаем от HH API список всех вакансий по тегу Ruby в регионе Санкт-Петербург
     response = open("https://api.hh.ru/vacancies?text=ruby&area=2").read
     vacancies_json = JSON.parse(response)
 
+    # Вытаскиваем из полученного списка id вакансий и сохраняем их в БД
     vacancies_json['items'].each do |elements|
       vacancy = Vacancy.new(vacancy_id: elements['id'])
       vacancy.save
--- app/jobs/vacancy_handling_job.rb
+++ app/jobs/vacancy_handling_job.rb
@@ -1,17 +1,20 @@
+# Обработка всех вакансий
+
 class VacancyHandlingJob
   @queue = :simple
 
   require 'open-uri'
 
   def self.perform
-
+    # Выбираем все вакансии из БД
     vacancies = Vacancy.all
 
     vacancies.each do |vacancy|
-
+      # Обрабатываем каждую вакансию по её id
       response = open("https://api.hh.ru/vacancies/#{vacancy.vacancy_id}").read
       vacancy_json = JSON.parse(response)
 
+      # Составляем hash-массив параметров вакансии
       vacancy_params = {
           name: vacancy_json['name'],
           description: vacancy_json['description'],
@@ -19,12 +22,14 @@ class VacancyHandlingJob
           company_name: vacancy_json['employer']['name'],
           company_link: vacancy_json['employer']['alternate_url']
       }
+      # т.к. з/п не всегда указана, то необходимо провести проверку
       unless vacancy_json['salary'].nil?
         vacancy_params[:salary_from] = vacancy_json['salary']['from']
         vacancy_params[:salary_to] = vacancy_json['salary']['to']
         vacancy_params[:currency] = vacancy_json['salary']['currency']
       end
 
+      # Обновляем поля вакансии
       vacancy.update(vacancy_params)
     end
   end
