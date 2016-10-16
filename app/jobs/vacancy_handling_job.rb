8094fb3b95d6d9f1a6f25d4a0aec6ee84768c16a
Alex <alex@alex-VirtualBox.(none)>
2016-10-16 15:03:20 +0300

    delete archived vacancies

--- app/jobs/vacancy_handling_job.rb
+++ app/jobs/vacancy_handling_job.rb
@@ -14,23 +14,27 @@ class VacancyHandlingJob
       response = open("https://api.hh.ru/vacancies/#{vacancy.vacancy_id}").read
       vacancy_json = JSON.parse(response)
 
-      # Составляем hash-массив параметров вакансии
-      vacancy_params = {
+      # Если вакансия в архиве, то удаляем её из БД
+      if vacancy_json['archived']
+        vacancy.destroy
+      else
+        # Составляем hash-массив параметров вакансии
+        vacancy_params = {
           name: vacancy_json['name'],
           description: vacancy_json['description'],
           link: vacancy_json['alternate_url'],
           company_name: vacancy_json['employer']['name'],
           company_link: vacancy_json['employer']['alternate_url']
-      }
-      # т.к. з/п не всегда указана, то необходимо провести проверку
-      unless vacancy_json['salary'].nil?
-        vacancy_params[:salary_from] = vacancy_json['salary']['from']
-        vacancy_params[:salary_to] = vacancy_json['salary']['to']
-        vacancy_params[:currency] = vacancy_json['salary']['currency']
+        }
+        # т.к. з/п не всегда указана, то необходимо провести проверку
+        unless vacancy_json['salary'].nil?
+          vacancy_params[:salary_from] = vacancy_json['salary']['from']
+          vacancy_params[:salary_to] = vacancy_json['salary']['to']
+          vacancy_params[:currency] = vacancy_json['salary']['currency']
+        end
+        # Обновляем поля вакансии
+        vacancy.update(vacancy_params)
       end
-
-      # Обновляем поля вакансии
-      vacancy.update(vacancy_params)
     end
   end
 end
